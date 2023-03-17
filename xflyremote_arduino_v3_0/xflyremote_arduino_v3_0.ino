1d196c4e0663075796ed38db29e017156f39613b
mydogspies <mydogspies@zoho.com>
2023-03-17 21:27:16 +0100

    Select and change value logic added

--- xflyremote_arduino_v3_0/xflyremote_arduino_v3_0.ino
+++ xflyremote_arduino_v3_0/xflyremote_arduino_v3_0.ino
@@ -33,7 +33,7 @@ int page_button_state[6][10] = {
   };
 int current_page_set = 0;
 
-// ROTZARY ENCODER LOGIC
+// ROTARY ENCODER LOGIC
 //
 #define rotaryCLK 6
 #define rotaryDT 7
@@ -46,6 +46,7 @@ int rotary_switch_state = 1;
 // RADIO PAGE LOGIC
 //
 // [page][number_box]
+// this one is purely for the internal logic of the select logic
 int radio_select_state[6][20] = {
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -54,6 +55,9 @@ int radio_select_state[6][20] = {
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 };
+// This global keeps track of which actual value box is currently selected
+String selectedbox;
+
 // transponder state array
 // only one single digit selected at one time
 // 0 - not active
@@ -61,12 +65,22 @@ int radio_select_state[6][20] = {
 int active_state_transp[] = {0, 0, 0, 0};
 // current frequencies in each position
 // note second item in the responder list is currently not used
-String radio_values[4][2] = {
-  {"118.000","136.990"},
-  {"108.000","117.950"},
-  {"0200","1750"},
-  {"7000","0"}
+// String radio_values[4][2] = {
+//  {"118.000","136.990"},
+//  {"108.000","117.950"},
+//  {"0200","1750"},
+//  {"7000","0"}
+// };
+
+String radiovalues[5][4] = {
+  {"120","000","130","500"},
+  {"110","000","115","100"},
+  {"0","1","9","0"},
+  {"1","2","5","0"},
+  {"7","7","7","7"}  
 };
+
+
 // id of img used for different states
 const String IMG_ID_NOSEL = "4"; // state 0
 const String IMG_ID_SEL = "9"; // state 0
@@ -120,6 +134,8 @@ void setup() {
   String con_text = "con.txt=\"Connected!\"";
   sendtonextion(con_text);
   sendtonextion(con_color);
+
+  
 }
 
 //
@@ -128,7 +144,7 @@ void setup() {
 void loop() {
 
   nextionlisten();
-  // readrotary();
+  readrotary();
 
 }
 
@@ -288,6 +304,7 @@ void pagetonextion(int page) {
   setbuttonstate(page); 
   if(page == 2) {
     setradiostate(page);
+    setradiovalues(page);
   }
 }
 
@@ -318,16 +335,15 @@ void radioselecttonextion(String cmd) {
   int page = cmd.substring(1,3).toInt();
   int imgbox = cmd.substring(3,5).toInt();
   int newvalue;
-
-  Serial.println(cmd);
-  Serial.println(page);
-  Serial.println(imgbox);
+  String vbox = cmd.substring(0,5);
 
   // check current value and remember new
   if(radio_select_state[page][imgbox] == 0) {
     newvalue = 1;
+    selectedbox = vbox;
   } else {
     newvalue = 0;
+    selectedbox = "0";
   }
 
   // reset array
@@ -338,7 +354,7 @@ void radioselecttonextion(String cmd) {
   // set to new value
   radio_select_state[page][imgbox] = newvalue;
   setradiostate(page);
-  
+  Serial.println(selectedbox);
 }
 
 
@@ -364,10 +380,36 @@ void setradiostate(int page) {
      } else {
       cmd = "page" + String(page) + ".i0" + String(page) + id + String(i) + ".picc=9"; 
     }
-    Serial.println(cmd);
     sendtonextion(cmd);
   }
+}
 
+// SET RADIO VALUES
+// reads the values array and set the radio frequency boxes
+void setradiovalues(int page) {
+  int i;
+  int k;
+  int id = 0;
+  String box;
+  String valb = "v0" + String(page);
+  String cmd;
+
+  // iterate through the radiovalues[][] array
+  for(i=0;i<5;i++) {
+    for(k=0;k<4;k++) {
+
+      // format with leading zero
+      if(id<10) {
+        box = valb + "0" + String(id);
+      } else {
+        box = valb + String(id);
+      }
+      // send value to nextion
+      cmd = "page" + String(page) + "." + box + ".val=" + radiovalues[i][k];
+      sendtonextion(cmd);
+      id++;
+    }
+  }
 }
 
 //
@@ -397,329 +439,17 @@ void buttontopython(String cmd) {
   sendtopython(bcmd);
 }
 
-
-
-
-// SEND RADIO FREQ TO PYTHON
-// format to send to xplane is;
-// rf XXX.XXX XXX.XXX YYY.YYY YYY.YYY AAAA AAAA TTTT
-// where XXX.XXX is coms 1 & 2 (118.000 for example), YYY.YYY are the navs
-// AAAA are the two ADFs and TTTT is transponder value
-void sendradiotoxplane() {
-  int i;
-  int k;
-  String sendstring = "rf";
-
-  for(i=0; i<4; i++) {
-
-    for(k=0; k<2; k++) {
-
-      if(i==3 && k==1) {
-        // do nothing, last value has no functio for now
-      } else {
-        sendstring = sendstring + " " + radio_values[i][k];
-      }
-
-    }
-
-  }
-  Serial.print(sendstring);
-
-}
-
-// send from radio_values[][] to display
-void setradiovalues() {
-
-  String freq;
-  String first;
-  String second;
-  String adf;
-  String trnsp;
-  String cmd = "";
-
-  int v = 1; // start address of the first text box
-  for (int i=0; i<2; i++) {
-
-    for (int j=0; j<2; j++) {
-
-      // get com & nav values and split them into integral and fractional
-      freq = radio_values[i][j];
-      for (int k=0; k<3; k++) {
-        first = first + freq[k];
-      }
-      for (int m=0; m<3; m++) {
-        second = second + freq[m+4];
-      }
-
-      cmd = "r" + String(v) + ".val=" + first;
-      nextion.print(cmd);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      cmd = "r" + String(v+1) + ".val=" + second;
-      nextion.print(cmd);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      v = v+2;
-      first = "";
-      second = "";
-      cmd = "";
-    }
-  }
-
-  // send adf valuies to nextion
-  v = 9; // start address for text box on the adf
-  for (int n=0; n<2; n++) {
-
-    adf = radio_values[2][n];
-    
-    for (int p=0; p<4; p++) {
-      char digit = adf[p];
-      cmd = "r" + String(v) + ".val=" + digit;
-      nextion.print(cmd);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      v ++;
-    }
-    adf = "";
-    cmd = "";
-  }
-
-  v = 17; // start address of the text box for trnsp
-  // int trnsp = int(radio_values[3][0]);
-  trnsp = radio_values[3][0];
-    
-  for (int q=0; q<4; q++) {
-    char digit = trnsp[q];
-    cmd = "r" + String(v) + ".val=" + digit;
-    nextion.print(cmd);
-    nextion.write(0xff);
-    nextion.write(0xff);
-    nextion.write(0xff);
-    v ++;
-    }
-    adf = "";
-    cmd = "";  
-}
-
-
-// SET RADIO PAGE
-// sets the radio active and selection graphics depending on current states array active_state_radios[][]
-void setradiodisplay() {
-  int i;
-  int k;
-  int idx = 0;
-  String cmd = "";
-
-  for (i=0; i<4; i++) {
-
-    for (k=0; k<4; k++) {
-
-      idx ++;
-      // jump for boxes with dual state but for independent digits
-      if(idx == 10) {idx = 11;}
-      if(idx == 12) {idx = 13;}
-      if(idx == 14) {idx = 15;}
-      if(idx == 16) {idx = 17;}
-
-      int sel = radio_select_state[i][k];
-
-      if(sel == 0) {
-        cmd = "sel" + String(idx) + ".picc=" + IMG_ID_NOSEL;
-      } else {
-        cmd = "sel" + String(idx) + ".picc=" + IMG_ID_SEL;
-      }
-
-      nextion.print(cmd);
-      nextion.write(0xff);
-      nextion.write(0xff);
-      nextion.write(0xff);
-    }
-    
-  } 
-
-} // END setradiodisplay()
-
 //
-// MAIN NEXTION LOOP
+// HARDWARE FUNCTIONS
 //
-// message format for buttons/touchspots is -> #LCppccdddd (for example '#7b05014848' means -> string (#) with (7) bytes of data attached (button 01 on page 05 with 4848 as data attached). 
-// # - means string of data follows
-// L - number of bytes
-// C - command (b - button, m - page
-// pp - page
-// cc - number
-// dddd - any data that follows
-// NOTE! the 'objname' in nextion tool attribute is the Cppnn part - so for example a button would be 'b0501' (button 01 on page 05)
-//
-void readnextion() {
 
-  /*
-  from_nextion = "";
-  int page = 0;
-  int button_id =0;
-  String cmd = "";
-  wait = 1;
-  */
-
-  // FROM NEXTION
-  if (nextion.available() > 2) {
-
-    char recv = nextion.read();
-
-    // Serial.println(recv);
-
-    // following code based on code from Athanasios Seitanis (CC Attrib 4.0 license)
-    // https://seithan.com/projects/nextion-tutorial/
-    //
-    // check if it's and incomming command
-    if(recv == "#") {
-      
-      uint8_t len = nextion.read(); // see how many bytes we expect
-      unsigned long wait1 = millis();
-      boolean found = true;
-
-      Serial.println(len);
-
-      // wait for buffer to fill out and time-out if not
-      while(nextion.available() < len) {
-        if((millis() - wait1) > 100) {
-          found = false;
-          break;
-        } 
-      }
-
-      // if we got all then do stuff to it
-      if(found == true) {
-        
-        int i;
-
-        for(i=0; i<len; i++) {
-          char stuff = nextion.read();
-          Serial.print(stuff);
-        }
-
-      }
-    }
-
-    
-    /*
-    // MENU COMMANDS
-    // incoming menu commands are as follows; m1 - ap, m2 - radio, m3 - lights, m4 - sys, m5 -utility
-    if(from_nextion.substring(0, 1) == "m") {
-
-      // check for sanity
-      if(from_nextion.length() == 2) {
-
-        int req = from_nextion.substring(1, 2).toInt();
-        page = page_sets[current_page_set][req-1];
-        String pg = "page " + String(page);
-        nextion.print(pg);
-        nextion.write(0xFF);
-        nextion.write(0xFF);
-        nextion.write(0xFF);
-
-        // if it is a radio page we need to recall the values since we use local vars in Nextion that reset between pages
-        if (req = 2) {
-        setradiodisplay();
-        setradiovalues();
-        }
-      }
-    }
-    */
-
-    /*
-    // BUTTON COMMANDS
-    // string from nextion has format;
-    // for buttons - b(page)(button_id) - example: b0100 (button, contains 01 and 00 (page 1, button_id 0) )
-    if(from_nextion.substring(0, 1) == "b") {
-
-      // check for sanity
-      if(from_nextion.length() == 5) {
-
-        page = from_nextion.substring(1, 3).toInt() - 1;
-        button_id = from_nextion.substring(3, 5).toInt();  
-
-        if (page_button_state[page][button_id] == 0) {
-          page_button_state[page][button_id] = 1;
-        } else {
-          page_button_state[page][button_id] = 0;
-        }
-   
-        String button_cmd = from_nextion + String(page_button_state[page][button_id]);
-        Serial.println(button_cmd);
-      }
-      // Serial.print(from_nextion);
-    }
-    */
-
-    /*
-    // RADIO SWAP BUTTONS
-    // string from nextion has format "s1" to "s3"
-    if(from_nextion.substring(0,1) == "s") {
-
-      // check for sanity
-      if(from_nextion.length() == 2) {
-        // read radio_values array and swap values
-        int idx = from_nextion.substring(1,2).toInt();
-        String val1 = radio_values[idx - 1][0];
-        String val2 = radio_values[idx - 1][1];
-        radio_values[idx - 1][0] = val2;
-        radio_values[idx - 1][1] = val1;
-        setradiovalues();
-
-      }
-      
-    }
-    */
-
-    /*
-    // RADIO SELECT FREQ BUTTONS
-    // the string format for the number boxes is "rXXpXX" where rXX is the number box and pXX is its place in the radio_select_state array
-    if(from_nextion.substring(0,1) == "r") {
-
-      if(from_nextion.length() == 6) {
-
-        int k;
-        int j;
-        String cmd = "";
-
-        int array_row = from_nextion.substring(4,5).toInt();
-        int array_column = from_nextion.substring(5,6).toInt();
-      
-        // look up current value and switch state
-        int state = radio_select_state[array_row][array_column];
-        if(state == 0) {
-          resetradiostate();
-          radio_select_state[array_row][array_column] = 1;        
-        } else {
-          resetradiostate();
-          radio_select_state[array_row][array_column] = 0;
-        }
-
-      setradiodisplay();
-      }
-    }
-    */
-
-  }
-}
-
-// simply resets radio_select_state array to all off
-void resetradiostate() {
-  int k;
-  int j;
-
-  for(k=0; k<4; k++) {
-    for(j=0; j<4; j++) {
-      radio_select_state[k][j] = 0;
-    }
-  }
-}
-
+// READ ROTARY
+// reads the rotary control
 void readrotary() {
+  int currentval;
+  int newval;
+  int boxid = selectedbox.substring(3,5).toInt();  
+  int page = selectedbox.substring(1,3).toInt();
 
   // SWITCH LOGIC
   int sw = digitalRead(rotarySW);
@@ -736,15 +466,40 @@ void readrotary() {
 
   // ROTARY LOGIC
   aState = digitalRead(rotaryCLK);
+
   if (aState != aLastState) {
-    if (digitalRead(rotaryDT) != aState) {
+    
+    if (digitalRead(rotaryDT) != aState) { // this is the positive side
+      Serial.println("pos");
+
+      // if there is selected box then find it in the array
+      if(selectedbox != "0") {
+        if(boxid < 4) {
+          newval = radiovalues[0][boxid].toInt() + 1;
+          // radiovalues[0][boxid] = String(newval);
+          
+        }    
+      }
+
       counter ++;
-    } else {
+
+    } else { // this is the negative side
+      Serial.println("neg");
+
+
+      if(selectedbox != "0") {
+        if(boxid < 3) {
+          newval = radiovalues[0][boxid].toInt() - 1;
+          // radiovalues[0][boxid] = String(newval);
+          
+        }    
+      }
       counter --;
+
     }
-    Serial.print("Counter: ");
-    Serial.println(counter);
+    // setradiovalues(page);
+
   }
   aLastState = aState;
+  // 
 } // END readrotary()
-
